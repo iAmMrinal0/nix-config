3ade01d4bfc22e66c064dd5a1bc69dfce3313989
Mrinal Purohit <github@mrinalpurohit.in>
2019-05-31 22:21:21 +0530

    Add chromium config

---
+++ nixpkgs/.config/nixpkgs/config/chromium.nix
@@ -0,0 +1,20 @@
+{
+  enable = true;
+  extensions = [
+    "fhgenkpocbhhddlgkjnfghpjanffonno" # Authy
+    "oboonakemofpalcgghocfoadofidjkkk" # Keepassxc Browser
+    "pkehgijcmpdhfbdbbnkijodmdjhbjlgp" # Privacy Badger
+    "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+    "edacconmaakjimmfgnblocblbcdcpbko" # Session Buddy
+    "hhinaapppaileiechjoiifaancjggfjm" # Web Scrobbler
+    "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
+    "bdakmnplckeopfghnlpocafcepegjeap" # Rescuetime
+    "hlepfoohegkhhmjieoechaddaejaokhf" # Refined GitHub
+    "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+    "omdakjcmkglenbhjadbccaookpfjihpa" # TunnelBear VPN
+    "neebplgakaahbhdphmkckjjcegoiijjo" # Keepa
+    "immpkjjlgappgfkkfieppnmlhakdmaab" # Imagus
+    "bkhaagjahfmjljalopjnoealnfndnagc" # Octotree
+    "mfgdmpfihlmdekaclngibpjhdebndhdj" # Humber New Tab Page
+  ];
+}
