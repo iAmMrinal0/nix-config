c391cf823cf2ca8200a71cde251901697dc2ae09
Mrinal Purohit <github@mrinalpurohit.in>
2019-06-28 18:01:11 +0530

    Enable hardware key support

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,6 +60,8 @@
     yubikey-personalization
   ];
 
+  hardware.u2f.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
