8b819b5529509d977bc94236788e90ad6196f48c
Mrinal Purohit <github@mrinalpurohit.in>
2019-09-12 19:37:33 +0530

    Add `cachix` for `all-hies` and `ghcide`(requires `hercules-ci`)

--- nixos/base.nix
+++ nixos/base.nix
@@ -5,6 +5,8 @@
 { config, pkgs, ... }:
 
 {
+  imports = [./cachix.nix];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
