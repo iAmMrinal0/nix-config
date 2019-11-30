7fa54cc68db3eb809211e44a34a6ed98363ca8fa
Mrinal Purohit <github@mrinalpurohit.in>
2019-12-01 04:05:40 +0530

    [vormir] Add init bluetooth config

--- nixos/hosts/vormir.nix
+++ nixos/hosts/vormir.nix
@@ -8,4 +8,12 @@
     ];
 
   networking.hostName = "vormir"; # Define your hostname.
+  hardware.bluetooth = {
+    enable = true;
+    extraConfig = "
+  [General]
+  Enable=Source,Sink,Media,Socket";
+  };
+  services.dbus.packages = [ pkgs.blueman ];
+
 }
