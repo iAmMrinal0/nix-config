9782935ac494b267ed22e636f01672830cf74621
Mrinal Purohit <github@mrinalpurohit.in>
2019-12-01 04:00:45 +0530

    [git] Fix ssh host not terminating with `/`

--- nixpkgs/.config/nixpkgs/config/git.nix
+++ nixpkgs/.config/nixpkgs/config/git.nix
@@ -14,8 +14,8 @@
     merge = { conflictstyle = "diff3"; tool = "ediff"; renormalize = true; };
     status = { showUntrackedFiles = "all"; };
     url = {
-      "ssh://git@bitbucket.org" = {insteadOf = "https://bitbucket.org/";};
-      "ssh://git@github.com" = {insteadOf = "https://github.com/";};
+      "ssh://git@bitbucket.org/" = {insteadOf = "https://bitbucket.org/";};
+      "ssh://git@github.com/" = {insteadOf = "https://github.com/";};
      };
     mergetool = {
       ediff = {cmd = "emacsclient -a '' --eval \"(ediff-merge-files-with-ancestor \\\"$LOCAL\\\" \\\"$REMOTE\\\" \\\"$BASE\\\" nil \\\"$MERGED\\\")\"";};
