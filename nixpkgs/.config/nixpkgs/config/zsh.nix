b753348f9f9b1315fe5c4e2fd27de65d26ed3e0d
Mrinal Purohit <github@mrinalpurohit.in>
2020-07-11 11:40:52 +0530

    [zsh] Add `command-not-found` and `zsh-syntax-highlighting` plugins

--- nixpkgs/.config/nixpkgs/config/zsh.nix
+++ nixpkgs/.config/nixpkgs/config/zsh.nix
@@ -22,23 +22,37 @@ with pkgs;
   };
   oh-my-zsh = {
     enable = true;
-    plugins = ["docker" "extract" "git" "kubectl" "sudo"];
+    plugins = ["command-not-found" "docker" "extract" "git" "kubectl" "sudo"];
     theme = "mod_steeef";
     custom = "${zshCustom}";
   };
-  plugins = [{
-    name = "zsh-autosuggestions";
-    src = fetchFromGitHub {
-      owner = "zsh-users";
-      repo = "zsh-autosuggestions";
-      rev = "v0.6.4";
-      sha256 = "0h52p2waggzfshvy1wvhj4hf06fmzd44bv6j18k3l9rcx6aixzn6";
-    };} {
-    name = "nix-zsh-completions";
-    src = fetchFromGitHub {
-      owner = "spwhitt";
-      repo = "nix-zsh-completions";
-      rev = "0.4.4";
-      sha256 = "1n9whlys95k4wc57cnz3n07p7zpkv796qkmn68a50ygkx6h3afqf";
-    };}];
+  plugins = [
+    {
+      name = "zsh-autosuggestions";
+      src = fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-autosuggestions";
+        rev = "v0.6.4";
+        sha256 = "0h52p2waggzfshvy1wvhj4hf06fmzd44bv6j18k3l9rcx6aixzn6";
+      };
+    }
+    {
+      name = "nix-zsh-completions";
+      src = fetchFromGitHub {
+        owner = "spwhitt";
+        repo = "nix-zsh-completions";
+        rev = "0.4.4";
+        sha256 = "1n9whlys95k4wc57cnz3n07p7zpkv796qkmn68a50ygkx6h3afqf";
+      };
+    }
+    {
+      name = "zsh-syntax-highlighting";
+      src = fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-syntax-highlighting";
+        rev = "0.7.1";
+        sha256 = "03r6hpb5fy4yaakqm3lbf4xcvd408r44jgpv4lnzl9asp4sb9qc0";
+      };
+    }
+  ];
 }
