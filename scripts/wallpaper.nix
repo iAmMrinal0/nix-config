91e5c44295bbd8e2738b518a274466f3c8353b1b
Mrinal Purohit <github@mrinalpurohit.in>
2020-07-13 16:34:08 +0530

    [wallpaper] Add the wallpaper to the repo
    Not fetching the wallpaper from remote because it get GC'ed (need to find the
    reason for this and how to prevent it)

--- scripts/wallpaper.nix
+++ scripts/wallpaper.nix
@@ -1,10 +1,7 @@
 { pkgs, ... }:
 
 with pkgs;
-let wallpaper = fetchurl {
-  url = "https://raw.githubusercontent.com/iAmMrinal0/dotfiles/master/wallpapers/kyloren.jpg";
-  sha256 = "8ca65c8513cbca0fe3655e6e312837a16172da7d69c73f9c1849b14288b47537";
-};
+let wallpaper = ../wallpapers/kyloren.jpg;
 in
 writeText "onAttachMonitor" ''
   ${feh}/bin/feh --bg-scale ${wallpaper}
