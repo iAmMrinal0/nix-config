847cd1ab0dbcfcce5d16ae36bf475bc321032ff0
Mrinal Purohit <github@mrinalpurohit.in>
2021-03-03 10:53:14 +0530

    [zsh] Add `zsh-nix-shell` plugin

--- config/zsh.nix
+++ config/zsh.nix
@@ -63,5 +63,15 @@ with pkgs;
         sha256 = "0y8va5kc2ram38hbk2cibkk64ffrabfv1sh4xm7pjspsba9n5p1y";
       };
     }
+    {
+      name = "zsh-nix-shell";
+      file = "nix-shell.plugin.zsh";
+      src = pkgs.fetchFromGitHub {
+        owner = "chisui";
+        repo = "zsh-nix-shell";
+        rev = "v0.1.0";
+        sha256 = "0snhch9hfy83d4amkyxx33izvkhbwmindy0zjjk28hih1a9l2jmx";
+      };
+    }
   ];
 }
