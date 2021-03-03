a52224a75aae5e4f69a8268fb62e9bcfbb1c5e54
Mrinal Purohit <github@mrinalpurohit.in>
2021-03-03 10:55:07 +0530

    [lock] Use `maim` instead of `scrot`

--- scripts/lock.nix
+++ scripts/lock.nix
@@ -5,11 +5,11 @@ with pkgs;
 let lock = ../locks/lock.png;
 in
 writeShellScript "lock" ''
-${scrot}/bin/scrot /tmp/screen.png
+${maim}/bin/maim --hidecursor /tmp/screen.png
 ${imagemagick}/bin/convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f ${lock} ]] && ${imagemagick}/bin/convert /tmp/screen.png ${lock} -gravity center -composite -matte /tmp/screen.png
-${i3lock}/bin/i3lock -u -i /tmp/screen.png
-rm /tmp/screen.png
-${pulseaudio}/bin/playerctl pause
+[[ -f ${lock} ]] && ${imagemagick}/bin/convert /tmp/screen.png ${lock} -gravity center -composite -matte /tmp/screen_pixel.png
+rm -f /tmp/screen.png
+${i3lock}/bin/i3lock -u -i /tmp/screen_pixel.png
+${playerctl}/bin/playerctl pause
 ${xorg.xset}/bin/xset dpms force off
 ''
