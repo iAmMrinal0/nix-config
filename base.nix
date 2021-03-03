c3790db9b79c1d38585e29838aeb1eedaa562891
Mrinal Purohit <github@mrinalpurohit.in>
2021-03-03 11:28:56 +0530

    [base] Update latest config
    - Use emacs GCC
    - Use latest home-manager
    - no password sudo for aws client vpn
    - add more fonts
    - use network manager instead of wireless, don't want to deal with cli for WiFi
      due to VPN requirements.
    - Add `openrazer` and `razergenie` for Razer devices

--- base.nix
+++ base.nix
@@ -2,12 +2,21 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, lib, pkgs, ... }:
+{ lib, pkgs, ... }:
 
-{
+let aws_client_vpn = pkgs.callPackage ./pkgs/aws_client_vpn { };
+
+in {
   imports = [
-    (import "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/release-20.03.tar.gz}/nixos")
+    (import "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos")
     ./cache.nix
+  ];
+
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+    }))
+    (import ./overlays.nix)
   ];
 
   # Use the systemd-boot EFI boot loader.
@@ -20,15 +29,22 @@
     chromium = { enableWideVine = true; };
   };
 
-  nix.extraOptions = ''
+  nix = {
+    autoOptimiseStore = true;
+    extraOptions = ''
     keep-outputs = true
-  '';
-
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    '';
+  };
 
   # Cloudflare DNS servers
-  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
-
+  networking = {
+    networkmanager = {
+      enable = true;
+      wifi.macAddress = "random";
+      dns = "none";
+    };
+    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  };
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -48,21 +64,82 @@
   environment.systemPackages = with pkgs; [
     atop
     android-file-transfer
-    avahi
+    aws_client_vpn
     binutils
-    blueman
     coreutils-full
     git
-    ntfs3g
     ncdu
+    ntfs3g
     openjdk
+    openssl
+    pptp
+    razergenie
     stow
-    tailscale
     tcpdump
     traceroute
     usbutils
     vim
     yubikey-personalization
+    (emacsWithPackagesFromUsePackage {
+      config = "${pkgs.fetchFromGitHub {
+        owner = "iammrinal0";
+        repo = ".emacs.d";
+        rev = "2532f617ab297678c9a582cdd425037ba3421375";
+        sha256 = "0a9dm7zhda8yv61r5is69knazbycircbg4jq3l5zingpn6wdhl0m";
+      }}/init.el";
+      package = pkgs.emacsGcc;
+      extraEmacsPackages = epkgs: (with epkgs; [
+        ace-window
+        ag
+        all-the-icons
+        anzu
+        avy
+        bind-key
+        dhall-mode
+        diminish
+        direnv
+        editorconfig
+        etcdctl
+        exec-path-from-shell
+        expand-region
+        flycheck
+        free-keys
+        git-gutter
+        groovy-mode
+        gruvbox-theme
+        haskell-mode
+        hasky-extensions
+        helm
+        helm-ag
+        helm-projectile
+        hungry-delete
+        hydra
+        keychain-environment
+        keyfreq
+        lsp-haskell
+        lsp-mode
+        lsp-ui
+        magit
+        markdown-mode
+        multiple-cursors
+        nix-buffer
+        nix-mode
+        org-bullets
+        pdf-tools
+        projectile
+        rainbow-delimiters
+        rainbow-mode
+        smart-mode-line
+        smartparens
+        use-package
+        web-mode
+        which-key
+        yaml-mode
+        yasnippet
+        zerodark-theme
+        zop-to-char
+      ]);
+    })
   ];
 
   environment.variables = {
@@ -83,20 +160,28 @@
   ];
 
   services.dbus.packages = [ pkgs.blueman ];
+  services.dnsmasq.enable = true;
+  services.emacs = {
+    enable = true;
+    package = pkgs.emacsGcc;
+    defaultEditor = true;
+    install = true;
+  };
+  services.etcd.enable = true;
   services.blueman.enable = true;
-
-  services.tailscale.enable = true;
+  services.upower.enable = true;
 
   hardware.bluetooth = {
     enable = true;
-    config = {
+    settings = {
       General = {
         Enable = "Source,Sink,Media,Socket";
       };
     };
+    package = pkgs.bluezFull;
   };
 
-  hardware.u2f.enable = true;
+  hardware.openrazer.enable = true;
 
   virtualisation.docker.enable = true;
 
@@ -104,17 +189,20 @@
   # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-  programs.ssh.startAgent = true;
-  programs.light.enable = true;
   programs.adb.enable = true;
+  programs.light.enable = true;
+  programs.nm-applet.enable = true;
+  programs.ssh.startAgent = true;
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPortRanges = [ { from=1714; to=1764; } ]; # KDE Connect Ports
+  networking.firewall.allowedUDPPortRanges = [ { from=1714; to=1764; } ]; # KDE Connect Ports
+  networking.firewall.allowedTCPPorts = [ 24800 ];
+  networking.firewall.allowedUDPPorts = [ 24800 1194 ]; # AWS Client VPN
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
@@ -130,11 +218,23 @@
     support32Bit = true;
   };
 
-  fonts.fonts = with pkgs; [
-    source-code-pro
-    iosevka
-    hasklig
-  ];
+  fonts = {
+    fonts = with pkgs; [
+      cantarell-fonts
+      dejavu_fonts
+      emacs-all-the-icons-fonts
+      # font-awesome_4
+      font-awesome
+      google-fonts
+      hasklig
+      iosevka
+      noto-fonts
+      source-code-pro
+    ];
+    fontconfig.enable = true;
+  };
+
+  services.fwupd.enable = true;
 
   services.xserver = {
     enable = true;
@@ -160,14 +260,22 @@
     };
   };
 
-
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.iammrinal0 = {
     isNormalUser = true;
-    extraGroups = [ "audio" "adbusers" "docker" "video" "plugdev" "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "adbusers" "audio" "docker" "networkmanager" "plugdev" "video" "wheel" ]; # Enable ‘sudo’ for the user.
     shell = pkgs.zsh;
   };
+
+  security.sudo.extraRules = [
+    { users = ["iammrinal0"];
+      commands = [
+        {command = "${aws_client_vpn}/bin/aws_client_vpn_connect"; options = ["NOPASSWD"]; }
+        {command = "${pkgs.openvpn_aws}/bin/openvpn"; options = ["NOPASSWD"]; }
+      ];
+    }
+  ];
+
   home-manager.users.iammrinal0 = ./home.nix;
   home-manager.useGlobalPkgs = true;
 
