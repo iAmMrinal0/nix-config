4aaf1e59781c992b4b38954400337b72092538c2
Mrinal Purohit <github@mrinalpurohit.in>
2021-06-03 01:05:43 +0530

    [emacs] Point to latest remote with PureScript changes

--- config/emacs.nix
+++ config/emacs.nix
@@ -4,8 +4,8 @@
   config = "${pkgs.fetchFromGitHub {
     owner = "iammrinal0";
     repo = ".emacs.d";
-    rev = "09f043b08f12c89d77272a7c16848a5f87858dec";
-    sha256 = "0w8frn5m47hl1xs9z2q6n7xp6sxl7yl321s0fq7pzs4zyvn7z5p6";
+    rev = "1d4ae437338cc8977d4a733f2c6b33cdac30ea9e";
+    sha256 = "1pz3lv58bd6q4zrcvyp9g2jk12gv0bdv78hjwgqgs6vxijcdaz4z";
   }}/init.el";
   package = pkgs.emacsGcc;
   extraEmacsPackages = epkgs: (with epkgs; [
@@ -48,6 +48,8 @@
     org-bullets
     pdf-tools
     projectile
+    psc-ide
+    purescript-mode
     rainbow-delimiters
     rainbow-mode
     smart-mode-line
