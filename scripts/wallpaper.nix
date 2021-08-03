7850bfd4a0f97268ce81f318c9be46fe3adc9bf8
Mrinal Purohit <github@mrinalpurohit.in>
2021-08-03 12:15:59 +0530

    [wallpaper] Change wallpaper to nix glow black

--- scripts/wallpaper.nix
+++ scripts/wallpaper.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 
-let wallpaper = ../wallpapers/kyloren.jpg;
+# wallpaper credits: https://github.com/gytis-ivaskevicius/high-quality-nix-content/blob/master/wallpapers/nix-glow-black.png
+let wallpaper = ../wallpapers/nix-glow-black.png;
 in pkgs.writeText "onAttachMonitor" ''
   ${pkgs.feh}/bin/feh --bg-scale ${wallpaper}
 ''
