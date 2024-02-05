e6874b2c59cbcaf360f268555dcb1f3d2dc70805
Mrinal Purohit <github@mrinalpurohit.in>
2024-02-05 16:52:13 +0100

    update git config

--- config/git.nix
+++ config/git.nix
@@ -2,20 +2,13 @@
   enable = true;
   userName = "Mrinal Purohit";
   userEmail = "github@mrinalpurohit.in";
-  includes = [{
-    contents = {
-      user.name = "Mrinal Purohit";
-      user.email = "mrinal.purohit@juspay.in";
-      pull.rebase = true;
-      rebase.autostash = true;
-    };
-    condition = "gitdir:~/play/";
-  }];
   extraConfig = {
     fetch = { prune = true; };
     core = { editor = "vim"; };
     rerere = { enabled = true; };
+    pull = { rebase = true; };
     push = { autoSetupRemote = true; };
+    rebase = { autostash = true; };
     merge = {
       conflictstyle = "diff3";
       tool = "ediff";
