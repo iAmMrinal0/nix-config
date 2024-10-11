f1577ed549f124cfed99d1e799fac69235dbaf5d
Mrinal Purohit <github@mrinalpurohit.in>
2024-10-11 21:39:07 +0200

    [rclone] init

--- system/linux.nix
+++ system/linux.nix
@@ -41,8 +41,8 @@ let
     pkgs.netcat-gnu
     pkgs.nix-diff
     pkgs.nixfmt-classic
-    pkgs.obs-studio
     pkgs.pgcli
+    pkgs.rclone
     pkgs.signing-party
     pkgs.ssh-to-pgp
     # pkgs.transmission-gtk
@@ -93,6 +93,31 @@ let
           Service.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
           Install.WantedBy = [ "default.target" ];
         };
+        rclone-gdrive-mount = {
+          Unit = {
+            Description = "Service that connects to Google Drive";
+            After = [ "default.target" ];
+            # Requires = [ "network.target" ];
+          };
+          Install = {
+            WantedBy = [ "default.target" ];
+          };
+
+          Service =
+            let
+              gdriveDir = "/home/iammrinal0/gdrive";
+            in
+            {
+              Type = "simple";
+              ExecStart = "${pkgs.rclone}/bin/rclone mount --vfs-cache-mode full gdrive: ${gdriveDir}";
+              ExecStop = "/run/current-system/sw/bin/fusermount -u ${gdriveDir}";
+              Restart = "on-failure";
+              RestartSec = "10s";
+              Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+
+            };
+        };
+
       };
     };
   };
