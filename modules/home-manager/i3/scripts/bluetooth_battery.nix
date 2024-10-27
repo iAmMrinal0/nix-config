1dd5a93e61d7eaf3c03d93be57403c412dc70420
Mrinal Purohit <github@mrinalpurohit.in>
2024-10-27 21:33:33 +0100

    update bluetooth battery script to show icon

--- modules/home-manager/i3/scripts/bluetooth_battery.nix
+++ modules/home-manager/i3/scripts/bluetooth_battery.nix
@@ -2,22 +2,23 @@
 
 pkgs.writeShellScript "bluetooth_battery" ''
   devices='[
-    {"device":"WH-1000XM3","serial":"CC:98:8B:F5:09:D3"},
-    {"device":"LE_WH-1000XM3","serial":"CC:98:8B:F5:09:D3"}
+    {"device":"WH-1000XM3","serial":"CC:98:8B:F5:09:D3","icon":""},
+    {"device":"LE_WH-1000XM3","serial":"CC:98:8B:F5:09:D3","icon":""}
   ]'
 
   # Iterate over each device in the JSON array
   for row in $(echo "$devices" | jq -c '.[]'); do
     model=$(echo "$row" | ${pkgs.jq}/bin/jq -r '.device')
     serial=$(echo "$row" | ${pkgs.jq}/bin/jq -r '.serial')
+    icon=$(echo "$row" | ${pkgs.jq}/bin/jq -r '.icon')
 
     # Get the charge percentage for the current device
     CHARGE=$(${pkgs.upower}/bin/upower --dump | ${pkgs.jc}/bin/jc --upower | ${pkgs.jq}/bin/jq -c --arg device "$model" --arg serial "$serial" \
       '.[] | select(.model==$device and .serial==$serial).detail.percentage | floor')
 
-    # Check if the charge percentage is valid and print it
+    # Check if the charge percentage is valid and print it with icon
     if [[ $CHARGE =~ ^[0-9]+$ ]] && (( CHARGE >= 0 && CHARGE <= 100 )); then
-      echo "$CHARGE%"
+      echo "$icon  $CHARGE%"
       break  # Exit after finding the charge for the first matched device
     fi
   done
