ecb65b63f42f30478a10e2fb81bad4a3e36aa8de
Mrinal Purohit <github@mrinalpurohit.in>
2025-01-17 10:49:25 +0100

    remove redundant config already setup by tailscale

--- base.nix
+++ base.nix
@@ -12,50 +12,51 @@ let
 
   vscodeExtensions = (with pkgs.vscode-extensions; [ github.copilot-chat ])
     ++ (with pkgs.vscode-marketplace;
-      [
-        ms-vscode-remote.vscode-remote-extensionpack
-        ms-vscode.remote-explorer
-        ms-vsliveshare.vsliveshare
-        ms-python.vscode-pylance
-        ms-python.python
-        github.copilot
-        pkief.material-icon-theme
-      ] ++ (with pkgs.open-vsx; [
-        mechatroner.rainbow-csv
-        ahmadalli.vscode-nginx-conf
-        bbenoist.nix
-        berberman.vscode-cabal-fmt
-        bierner.markdown-mermaid
-        bigmoon.language-yesod
-        davidanson.vscode-markdownlint
-        dhall.dhall-lang
-        dhall.vscode-dhall-lsp-server
-        dksedgwick.xstviz
-        eamodio.gitlens
-        editorconfig.editorconfig
-        github.vscode-pull-request-github
-        hashicorp.terraform
-        haskell.haskell
-        jdinhlife.gruvbox
-        jnoortheen.nix-ide
-        jock.svg
-        joeandaverde.sqitch-plan
-        justusadam.language-haskell
-        miguelsolorio.fluent-icons
-        mkhl.direnv
-        william-voyek.vscode-nginx
-        ms-azuretools.vscode-docker
-        ms-python.black-formatter
-        ms-vscode-remote.remote-ssh
-        ms-vsliveshare.vsliveshare
-        raynigon.nginx-formatter
-        redhat.vscode-yaml
-        statelyai.stately-vscode
-        # vscodeemacs.emacs
-        lfs.vscode-emacs-friendly
-        graphql.vscode-graphql-syntax
-        tootone.org-mode
-      ]));
+    [
+      ms-vscode-remote.vscode-remote-extensionpack
+      ms-vscode.remote-explorer
+      ms-vsliveshare.vsliveshare
+      ms-python.vscode-pylance
+      ms-python.python
+      github.copilot
+      pkief.material-icon-theme
+    ] ++ (with pkgs.open-vsx; [
+      mechatroner.rainbow-csv
+      ahmadalli.vscode-nginx-conf
+      bbenoist.nix
+      berberman.vscode-cabal-fmt
+      bierner.markdown-mermaid
+      bigmoon.language-yesod
+      davidanson.vscode-markdownlint
+      dhall.dhall-lang
+      dhall.vscode-dhall-lsp-server
+      dksedgwick.xstviz
+      eamodio.gitlens
+      editorconfig.editorconfig
+      github.vscode-pull-request-github
+      hashicorp.terraform
+      haskell.haskell
+      jdinhlife.gruvbox
+      jnoortheen.nix-ide
+      jock.svg
+      joeandaverde.sqitch-plan
+      justusadam.language-haskell
+      miguelsolorio.fluent-icons
+      mkhl.direnv
+      william-voyek.vscode-nginx
+      ms-azuretools.vscode-docker
+      ms-python.black-formatter
+      ms-vscode-remote.remote-ssh
+      ms-vsliveshare.vsliveshare
+      raynigon.nginx-formatter
+      redhat.vscode-yaml
+      statelyai.stately-vscode
+      # vscodeemacs.emacs
+      lfs.vscode-emacs-friendly
+      graphql.vscode-graphql-syntax
+      tootone.org-mode
+      tailscale.vscode-tailscale
+    ]));
 
   vscode-with-extensions = pkgs.vscode-with-extensions.override {
     vscodeExtensions = vscodeExtensions ++ [
@@ -69,7 +70,8 @@ let
       }).overrideAttrs (_: { sourceRoot = null; }))
     ];
   };
-in {
+in
+{
 
   sops = {
     defaultSopsFile = ./sops/secrets.yaml;
@@ -199,7 +201,11 @@ in {
     libinput = { enable = true; };
     gvfs = { enable = true; };
     gnome.gnome-keyring.enable = true;
-    tailscale.enable = true;
+    tailscale = {
+      enable = true;
+      openFirewall = true;
+      useRoutingFeatures = "both";
+    };
     pipewire = {
       enable = true;
       pulse.enable = true;
@@ -234,18 +240,9 @@ in {
 
   networking = {
     firewall = {
-      allowedTCPPortRanges = [{
-        from = 1714;
-        to = 1764;
-      }]; # KDE Connect Ports
-      allowedUDPPortRanges = [{
-        from = 1714;
-        to = 1764;
-      }]; # KDE Connect Ports
       allowedTCPPorts = [ 24800 22 ];
-      allowedUDPPorts = [ 24800 config.services.tailscale.port ];
+      allowedUDPPorts = [ 24800 ];
       trustedInterfaces = [ "tailscale0" ];
-      checkReversePath = "loose";
     };
     networkmanager = {
       enable = true;
