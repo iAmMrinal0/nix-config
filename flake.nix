6de7be62e50b9c034c3b1d8717adcb54cbf6eb7a
Mrinal Purohit <github@mrinalpurohit.in>
2025-01-30 11:06:49 +0100

    update flake.lock, remove ghostty

--- flake.nix
+++ flake.nix
@@ -26,13 +26,12 @@
     haskell-yesod-quasiquotes.url =
       "github:kronor-io/haskell-yesod-quasiquotes";
     haskell-yesod-quasiquotes.flake = false;
-    ghostty.url = "github:ghostty-org/ghostty";
   };
 
   outputs = inputs@{ self, nixpkgs, nur, home-manager, sops-nix, emacs-overlay
     , nixos-hardware, emacsConfiguration, nix-vscode-extensions
     , zsh-autosuggestions, zsh-you-should-use, zsh-history-substring-search
-    , zsh-nix-shell, keepmenu, haskell-yesod-quasiquotes, ghostty }: {
+    , zsh-nix-shell, keepmenu, haskell-yesod-quasiquotes }: {
       nixosConfigurations.betazed = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
