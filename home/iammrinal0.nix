67875033bdc305913e4b60a555c8b3b5ee7cd72b
Mrinal Purohit <github@mrinalpurohit.in>
2025-02-21 11:03:34 +0100

    Reorganize home nix files

---
+++ home/iammrinal0.nix
@@ -0,0 +1,58 @@
+{ pkgs, config, lib, inputs, ... }:
+let
+  userPackages = import ./packages.nix { inherit pkgs lib inputs; };
+in {
+  xdg = {
+    enable = true;
+    configFile."pgcli/config".text = builtins.readFile ../config/pgcli;
+  };
+
+  dconf.settings."gnome/desktop/sound" = { 
+    event-sounds = false; 
+  };
+
+  services = {
+    gpg-agent = {
+      enable = true;
+      pinentryPackage = pkgs.pinentry-qt;
+    };
+    kdeconnect = {
+      enable = true;
+      indicator = true;
+    };
+    playerctld.enable = true;
+    udiskie.enable = true;
+  };
+
+  programs = {
+    atuin = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    broot.enable = true;
+    command-not-found.enable = true;
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    gpg.enable = true;
+    home-manager.enable = true;
+    jq.enable = true;
+  };
+
+  home = {
+    packages = userPackages;
+    stateVersion = "24.05";
+    sessionVariables = {
+      SSH_AUTH_SOCK = "${config.xdg.dataHome}/ssh-agent";
+      BITWARDEN_SSH_AUTH_SOCK = "${config.home.sessionVariables.SSH_AUTH_SOCK}";
+    };
+  };
+
+  imports = [ ../modules/home-manager ];
+}
+
---
+++ home/packages.nix
@@ -0,0 +1,123 @@
+{ pkgs, lib, inputs }:
+with pkgs; [
+  # Development Tools
+  eza
+  cachix
+  gnumake
+  nixpkgs-review
+  nmap
+  git-crypt
+  pciutils
+  silver-searcher
+  awscli
+  shellcheck
+  nixfmt-classic
+  gh
+  nil
+  btop
+
+  # Text Processing & Search
+  ripgrep
+  tree
+  yq
+  jq
+
+  # System Utils
+  coreutils-full
+  dnsutils
+  pv
+  lshw
+  acpi
+  lsof
+  netcat-gnu
+  nix-diff
+  xorg.xkill
+  nvd
+
+  # Security & Authentication
+  keepassxc
+  signing-party
+  ssh-to-pgp
+  openvpn
+
+  # Development Environment
+  (emacsWithPackagesFromUsePackage (import ../config/emacs.nix { inherit inputs pkgs; }))
+  sqlite
+  pgcli
+  rlwrap
+  nodejs
+  (lib.hiPrio insomnia)
+
+  # Desktop Environment
+  terminator
+  google-chrome
+  slack
+  obsidian
+  spotify
+  vlc
+  discord
+  nautilus
+  ranger
+
+  # Media & Graphics
+  imagemagick
+  ffmpeg-full
+  gnome-screenshot
+
+  # File Management
+  xarchiver
+  xfce.thunar
+  xfce.thunar-volman
+  xfce.thunar-archive-plugin
+  xfce.tumbler
+  xfce.xfconf
+  rclone
+
+  # System Appearance
+  lxappearance
+  arc-theme
+  adwaita-icon-theme
+  papirus-icon-theme
+  paper-icon-theme
+
+  # Clipboard & Input
+  xclip
+  xsel
+  xdotool
+
+  # Audio
+  alsa-utils
+  pulseaudio
+  pavucontrol
+  playerctl
+
+  # Display
+  xorg.xdpyinfo
+  arandr
+  xorg.libxcvt
+
+  # Notification
+  libnotify
+  dunst
+
+  # Qt & Desktop Integration
+  libsForQt5.qtstyleplugins
+  libsForQt5.qt5ct
+  dconf
+  dconf-editor
+
+  # Custom inputs
+  inputs.keepmenu
+
+  # Language & Spellcheck
+  aspell
+  aspellDicts.en
+  aspellDicts.en-computers
+
+  # Network & Communication
+  transmission_4-gtk
+
+  # System Information
+  neofetch
+  nix-prefetch-github
+]
