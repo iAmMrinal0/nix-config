dac8f9986e0d09e714ceb2b7a68ecab2daf215a1
Mrinal Purohit <github@mrinalpurohit.in>
2025-07-05 21:39:35 +0200

    add pgcli environment based config

--- modules/home/xdg/main.nix
+++ modules/home/xdg/main.nix
@@ -7,6 +7,8 @@
     xdg = {
       enable = true;
       configFile."pgcli/config".text = builtins.readFile ../../../config/pgcli;
+      configFile."pgcli/pgcli-prod".text = builtins.readFile ../../../config/pgcli-prod;
+      configFile."pgcli/pgcli-staging".text = builtins.readFile ../../../config/pgcli-staging;
     };
   };
 }
