d04894a317c2bb024c48bb55ef1d62aeee72f3fc
Mrinal Purohit <github@mrinalpurohit.in>
2025-07-10 16:20:46 +0200

    obsidian: add webgpu flag

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -1,6 +1,15 @@
-self: super: {
+self: super:
+{
   scripts = import ../pkgs/scripts { pkgs = super; };
 
   crow = super.callPackage ../pkgs/crow { };
   huenicorn = super.callPackage ../pkgs/huenicorn { inherit (self) crow; };
-}
+} // (if super ? obsidian then {
+  # Only apply the Obsidian wrapper if obsidian package exists
+  obsidian = super.obsidian.overrideAttrs (oldAttrs: {
+    postInstall = (oldAttrs.postInstall or "") + ''
+      wrapProgram $out/bin/obsidian --add-flags "--enable-unsafe-webgpu --lang=en-gb"
+    '';
+  });
+} else
+  { })
